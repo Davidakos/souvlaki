2b00ad79c53121a4830cb469cd813a0a9e2b2e7c
Davidakos <squadblastersdy@gmail.com>
2025-03-20 12:45:43 +0100

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-// JavaScript file to handle any interactive functionality
-// For now, the page is static but you can add dynamic functionality later.
-console.log("GitHub Clone Loaded!");
+// JavaScript to add some interaction if needed
+document.querySelector('.cta-btn').addEventListener('click', () => {
+    alert('Getting Started!');
+});
+
